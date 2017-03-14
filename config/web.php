4c7b046e0027000446953b84d31829b7e9b4fe7d
AlexanderPavlenko94 <alex.pavlenko.94@gmail.com>
2017-03-14 15:42:03 +0200

    Fix web

--- config/web.php
+++ config/web.php
@@ -58,7 +58,6 @@ $config = [
         'db' => require(__DIR__ . '/db.php'),
         'urlManager' => [
             'enablePrettyUrl' => true,
-            //'enableStrictParsing' => true,
             'showScriptName' => false,
             'rules' => $routes
         ],
